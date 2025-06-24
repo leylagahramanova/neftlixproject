5e7d9e111a8a0fc0eb8ad2e95338eb7f9aa113f5
leylagahramanova <leylagahramanova13@gmail.com>
2025-06-24 14:45:53 +0400

    3456789

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
